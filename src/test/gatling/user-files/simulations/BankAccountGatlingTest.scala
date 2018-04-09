7d06234db32ea3af6b13d003ac4d3c11b11c45e0
yogendra8singh <yogendra8singh@gmail.com>
2018-04-09 23:24:30 +0530

    Update BankAccountGatlingTest.scala

--- src/test/gatling/user-files/simulations/BankAccountGatlingTest.scala
+++ src/test/gatling/user-files/simulations/BankAccountGatlingTest.scala
@@ -18,7 +18,7 @@ class BankAccountGatlingTest extends Simulation {
     //context.getLogger("io.gatling.http").setLevel(Level.valueOf("DEBUG"))
 
     //val baseURL = Option(System.getProperty("baseURL")) getOrElse """http://127.0.0.1:8080"""
-    val baseURL = Option(System.getProperty("baseURL")) getOrElse """https://web.devdemo.xyz"""
+    val baseURL = Option(System.getProperty("baseURL")) getOrElse """https://35.168.203.14"""
 
     val httpConf = http
         .baseURL(baseURL)
